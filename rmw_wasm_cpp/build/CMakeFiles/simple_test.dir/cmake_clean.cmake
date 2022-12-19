f1f59b9e7a87e258ec8a738ef64652279873091d
Isabel Paredes <iparedes314@gmail.com>
2022-12-19 14:12:11 +0100

    Clean up

---
+++ rmw_wasm_cpp/build/CMakeFiles/simple_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_test.dir/src/simple_test.cpp.o"
+  "CMakeFiles/simple_test.dir/src/simple_test.cpp.o.d"
+  "simple_test.html"
+  "simple_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
