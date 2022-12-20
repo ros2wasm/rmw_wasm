ca2508e0ea25ab3e839443a634b80a288f09f633
Isabel Paredes <iparedes314@gmail.com>
2022-12-20 20:31:15 +0100

    Add identifier and force static

---
+++ rmw_wasm_cpp/include/identifier.hpp
@@ -0,0 +1,13 @@
+#ifndef RMW_WASM_CPP__IDENTIFIER_HPP_
+#define RMW_WASM_CPP__IDENTIFIER_HPP_
+
+namespace rmw_wasm_cpp
+{
+
+    extern const char * identifier;
+    // TODO:
+    // extern const char * serialization_format;
+
+} // namespace rmw_wasm_cpp
+
+#endif // RMW_WASM_CPP__IDENTIFIER_HPP_
---
+++ rmw_wasm_cpp/src/identifier.cpp
@@ -0,0 +1,10 @@
+#include "identifier.hpp"
+
+namespace rmw_wasm_cpp
+{
+
+    const char * identifier = "rmw_wasm_cpp";
+    // TODO:
+    // const char * serialization_format = "json?";
+
+} // namespace rmw_wasm_cpp
