98254f98fa7fdf3b55665b2158faee6c7a6628d5
Isabel Paredes <iparedes314@gmail.com>
2022-12-23 18:47:54 +0100

    Add unimplemented serialize functions

---
+++ rmw_wasm_cpp/src/rmw_serialize.cpp
@@ -0,0 +1,39 @@
+#include <iostream> // REMOVE
+
+#include "rmw/rmw.h"
+#include "rmw/error_handling.h"
+
+extern "C"
+{
+
+    rmw_ret_t rmw_get_serialized_message_size(
+        [[maybe_unused]] const rosidl_message_type_support_t * type_support,
+        [[maybe_unused]] const rosidl_runtime_c__Sequence__bound * message_bounds,
+        [[maybe_unused]] size_t * size
+    )
+    {
+        RMW_SET_ERROR_MSG("rmw_get_serialized_message_size not implemented");
+        return RMW_RET_ERROR;
+    }
+
+    rmw_ret_t rmw_serialize(
+        [[maybe_unused]] const void * ros_message,
+        [[maybe_unused]] const rosidl_message_type_support_t * type_support,
+        [[maybe_unused]] rmw_serialized_message_t * serialized_message)
+    {
+        // TODO: figure out if needed
+        RMW_SET_ERROR_MSG("rmw_serialize not implemented");
+        return RMW_RET_ERROR;
+    }
+
+    rmw_ret_t rmw_deserialize(
+        [[maybe_unused]] const rmw_serialized_message_t * serialized_message,
+        [[maybe_unused]] const rosidl_message_type_support_t * type_support,
+        [[maybe_unused]] void * ros_message)
+    {
+        // TODO: figure out if needed
+        RMW_SET_ERROR_MSG("rmw_deserialize not implemented");
+        return RMW_RET_ERROR;
+    }
+
+} // extern "C"
