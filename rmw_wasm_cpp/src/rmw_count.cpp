e8095a52daeb8806644956e19012c34eea44f8d7
Isabel Paredes <iparedes314@gmail.com>
2022-12-25 01:06:59 +0100

    Add pub and sub count

---
+++ rmw_wasm_cpp/src/rmw_count.cpp
@@ -0,0 +1,46 @@
+#include "rmw_wasm_cpp/identifier.hpp"
+
+#include "rmw/rmw.h"
+#include "rmw/impl/cpp/macros.hpp"
+
+extern "C"
+{
+
+    static rmw_ret_t _count_entities(
+        const rmw_node_t * node,
+        const char * topic_name,
+        size_t * count)
+    {
+        RMW_CHECK_ARGUMENT_FOR_NULL(node, RMW_RET_INVALID_ARGUMENT);
+        RMW_CHECK_TYPE_IDENTIFIERS_MATCH(
+            node,
+            node->implementation_identifier,
+            rmw_wasm_cpp::identifier,
+            return RMW_RET_INCORRECT_RMW_IMPLEMENTATION);
+        RMW_CHECK_ARGUMENT_FOR_NULL(topic_name, RMW_RET_INVALID_ARGUMENT);
+        // TODO: validate topic name - rmw_wasm_cpp::is_valid_topic_name()
+
+        RMW_CHECK_ARGUMENT_FOR_NULL(count, RMW_RET_INVALID_ARGUMENT);
+        // TODO: figure out if needed
+        
+        *count = 1u;
+        return RMW_RET_OK;
+    }
+
+    rmw_ret_t rmw_count_publishers(
+        const rmw_node_t * node,
+        const char * topic_name,
+        size_t * count)
+    {
+        return _count_entities(node, topic_name, count);
+    }
+
+    rmw_ret_t rmw_count_subscribers(
+        const rmw_node_t * node,
+        const char * topic_name,
+        size_t * count)
+    {
+        return _count_entities(node, topic_name, count);
+    }
+
+} // extern "C"
