4131c80b3920b1f4810bb620099b880154068439
Isabel Paredes <iparedes314@gmail.com>
2022-12-25 01:56:58 +0100

    Add log and features

---
+++ rmw_wasm_cpp/src/rmw_log_and_features.cpp
@@ -0,0 +1,22 @@
+#include "rmw_wasm_cpp/identifier.hpp"
+
+#include "rmw/rmw.h"
+#include "rmw/features.h"
+
+extern "C"
+{
+    rmw_ret_t rmw_set_log_severity(
+        [[maybe_unused]] rmw_log_severity_t severity)
+    {
+        // TODO: implement
+        return RMW_RET_OK;
+    }
+
+    bool rmw_feature_supported(
+        [[maybe_unused]] rmw_feature_t feature)
+    {
+        // TODO: implement
+        return true;
+    }
+
+}
