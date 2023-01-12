cbf2489855f8d96fb528076b6621d0a2c62c558a
Isabel Paredes <iparedes314@gmail.com>
2023-01-12 13:30:00 +0100

    Modify publisher

--- wasm_cpp/src/publisher.cpp
+++ wasm_cpp/src/publisher.cpp
@@ -9,21 +9,22 @@ namespace wasm_cpp
     Publisher::Publisher(const std::string & topic_name)
     {
         // TODO: create publisher
-        std::cout << "[WASM] Publisher created\n"; // REMOVE
+        std::cout << " [PUB] Publisher created\n"; // REMOVE
         std::cout << " [PUB] topic: " << topic_name << '\n';
     }
 
     Publisher::~Publisher()
     {
         // TODO: destroy publisher
-        std::cout << "[WASM] Publisher destroyed\n"; // REMOVE
+        std::cout << " [PUB] Publisher destroyed\n"; // REMOVE
     }
 
-    void Publisher::publish(const std::string & message)
+    void Publisher::publish(
+        [[maybe_unused]] const std::string & message)
     {
         // TODO: call js
-        std::cout << "[WASM] Publishing a message\n"; // REMOVE
-        std::cout << " [PUB] msg: " << message << '\n';
+        std::cout << " [PUB] Publishing a message\n"; // REMOVE
+        // std::cout << " [PUB] msg: " << message << '\n';
     }
 
 } // namespace wasm_cpp
