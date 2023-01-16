e6d0e4073c986fc487429228a8ea0d7c0522bd15
Isabel Paredes <iparedes314@gmail.com>
2023-01-16 17:44:52 +0100

    Add publisher test

---
+++ test_pubsub/src/publisher_member_function.cpp
@@ -0,0 +1,65 @@
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include <unistd.h>
+#include <iostream>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+ * member function as a callback from the timer. */
+
+class MinimalPublisher : public rclcpp::Node
+{
+public:
+  MinimalPublisher()
+  : Node("wasm_publisher"), count_(0)
+  {
+    publisher_ = this->create_publisher<std_msgs::msg::String>("wasm_topic", 10);
+    timer_ = this->create_wall_timer(
+      1000ms, std::bind(&MinimalPublisher::timer_callback, this));
+  }
+
+// private:
+  void timer_callback()
+  {
+    auto message = std_msgs::msg::String();
+    message.data = "Hello there! " + std::to_string(count_++);
+    // RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    std::string msg_str{ message.data };
+    std::cout << " [MSG] " << msg_str << '\n';
+    publisher_->publish(message);
+    usleep(500 * 1000); // microseconds
+  }
+
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+
+  rclcpp::shutdown();
+  return 0;
+}
