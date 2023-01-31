1190a071854b093102056f1f7d9f60ce26010d03
Isabel Paredes <iparedes314@gmail.com>
2023-01-31 15:11:37 +0100

    Add test without asincify

---
+++ test_py_pubsub/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'test_py_pubsub'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ihuicatl',
+    maintainer_email='isabel.paredes@quantstack.net',
+    description='Minimal publisher',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = py_pubsub.publisher_member_function:main',
+        ],
+    },
+)
