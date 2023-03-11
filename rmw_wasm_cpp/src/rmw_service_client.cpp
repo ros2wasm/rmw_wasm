9f03772f4da247b8477f98d4b0c58831c0a1b3df
Isabel Paredes <iparedes314@gmail.com>
2023-03-11 22:33:21 +0100

    Reorganize files

--- rmw_wasm_cpp/src/rmw_service_client.cpp
+++ rmw_wasm_cpp/src/rmw_service_client.cpp
@@ -19,7 +19,7 @@ extern "C"
         const rmw_node_t * node,
         const rosidl_service_type_support_t * type_supports,
         const char * service_name,
-        const rmw_qos_profile_t * qos_policies)
+        const rmw_qos_profile_t * qos_profile)
     {
         RCUTILS_LOG_DEBUG_NAMED("rmw_wasm_cpp", "trace rmw_create_client()");
 
@@ -37,7 +37,7 @@ extern "C"
         }
         // TODO: validate service name rmw_wasm_cpp::is_valid_service_name()
 
-        RMW_CHECK_ARGUMENT_FOR_NULL(qos_policies, nullptr);
+        RMW_CHECK_ARGUMENT_FOR_NULL(qos_profile, nullptr);
         // TODO: validate qos policy rmw_wasm_cpp::is_valid_qos()
 
         // TODO: get type support from rmw if needed
@@ -125,6 +125,61 @@ extern "C"
 
         RMW_CHECK_ARGUMENT_FOR_NULL(rmw_client, RMW_RET_INVALID_ARGUMENT);
         // TODO: implement if needed
+        return RMW_RET_OK;
+    }
+
+    rmw_ret_t rmw_send_request(
+        const rmw_client_t * client,
+        const void * ros_request,
+        int64_t * sequence_id)
+    {
+        RCUTILS_LOG_DEBUG_NAMED("rmw_wasm_cpp", "trace rmw_send_request()");
+
+        RMW_CHECK_ARGUMENT_FOR_NULL(client, RMW_RET_INVALID_ARGUMENT);
+        RMW_CHECK_TYPE_IDENTIFIERS_MATCH(
+            client,
+            client->implementation_identifier,
+            rmw_wasm_cpp::identifier,
+            return RMW_RET_INCORRECT_RMW_IMPLEMENTATION);
+        RMW_CHECK_ARGUMENT_FOR_NULL(ros_request, RMW_RET_INVALID_ARGUMENT);
+        RMW_CHECK_ARGUMENT_FOR_NULL(sequence_id, RMW_RET_INVALID_ARGUMENT);
+
+        // TODO: create rmw_wasm_client
+        // TODO: create wasm_client
+        
+        // TODO: conver message to JSON???
+        // TODO: send request
+        
+        return RMW_RET_OK;
+    }
+
+    rmw_ret_t rmw_take_response(
+        const rmw_client_t * client,
+        rmw_service_info_t * request_header,
+        void * ros_response,
+        bool * taken)
+    {
+        RCUTILS_LOG_DEBUG_NAMED("rmw_wasm_cpp", "trace rmw_take_response()");
+
+        RMW_CHECK_ARGUMENT_FOR_NULL(client, RMW_RET_INVALID_ARGUMENT);
+        RMW_CHECK_TYPE_IDENTIFIERS_MATCH(
+            client,
+            client->implementation_identifier,
+            rmw_wasm_cpp::identifier,
+            return RMW_RET_INCORRECT_RMW_IMPLEMENTATION);
+        RMW_CHECK_ARGUMENT_FOR_NULL(request_header, RMW_RET_INVALID_ARGUMENT);
+        RMW_CHECK_ARGUMENT_FOR_NULL(ros_response, RMW_RET_INVALID_ARGUMENT);
+        RMW_CHECK_ARGUMENT_FOR_NULL(taken, RMW_RET_INVALID_ARGUMENT);
+
+        // TODO: create rmw_wasm_client
+        // TODO: create wasm_client
+        
+        // TODO: get info 
+        // suggetion: wasm_client->get_response_with_info()
+        
+        // TODO: convert JSON back to response
+        // TODO: copy info to response header
+        
         return RMW_RET_OK;
     }
 
