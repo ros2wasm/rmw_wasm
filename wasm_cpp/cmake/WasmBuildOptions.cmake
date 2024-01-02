5edd99708902d296ed9c21491418c1fd70aee095
Tobias Fischer <info@tobiasfischer.info>
2024-01-02 13:43:51 +1000

    Progress

--- wasm_cpp/cmake/WasmBuildOptions.cmake
+++ wasm_cpp/cmake/WasmBuildOptions.cmake
@@ -4,6 +4,7 @@ function(wasm_compile_options target)
     target_compile_options("${target}"
         PUBLIC "SHELL: -fexceptions"
     )
+    target_compile_options("${target}" PUBLIC -fPIC)
 endfunction()
 
 function(wasm_link_options target)
@@ -11,7 +12,7 @@ function(wasm_link_options target)
         PUBLIC "SHELL: -lembind"
         PUBLIC "SHELL: --pre-js ${CMAKE_SOURCE_DIR}/pre.js"
         PUBLIC "SHELL: -fexceptions"
-        PUBLIC "SHELL: -s ASYNCIFY=1"
+        # PUBLIC "SHELL: -s ASYNCIFY=1"
         PUBLIC "SHELL: -s ALLOW_MEMORY_GROWTH=1"
         PUBLIC "SHELL: -s DEMANGLE_SUPPORT=1"
         PUBLIC "SHELL: -s ASSERTIONS=1"
@@ -20,7 +21,7 @@ function(wasm_link_options target)
         PUBLIC "SHELL: -s USE_PTHREADS=0"
         PUBLIC "SHELL: -s TOTAL_STACK=16mb"
         PUBLIC "SHELL: -s INITIAL_MEMORY=64mb"
-        PUBLIC "SHELL: -s ASYNCIFY_STACK_SIZE=24576"
-        PUBLIC "SHELL: -03" # Optimize for async
+        # PUBLIC "SHELL: -s ASYNCIFY_STACK_SIZE=24576"
+        # PUBLIC "SHELL: -03" # Optimize for async
     )
 endfunction()
--- wasm_cpp/src/pre.js
+++ wasm_cpp/src/pre.js
@@ -1,29 +1,76 @@
-
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-Module["registerParticipant"] = function registerParticipant(name, role)
-{
+const msgMap = {};
 
-    return gid;
-}
+// When a new message is received from main
+self.onmessage = function (event) {
+  if (
+    event.data &&
+    typeof event.data === "object" &&
+    "topic" in event.data &&
+    "message" in event.data
+  ) {
+    msgMap[event.data.topic] = {
+      message: event.data.message.replaceAll(", ", "\n"),
+      isStale: false,
+    };
+  }
+};
 
-Module["deregisterParticipant"] = function deregisterParticipant(gid)
-{
-    return;
-}
+Module["registerParticipant"] = function registerParticipant(topic_name, role) {
+  topic = topic_name;
+  let gid = Math.random().toString(16).slice(2);
 
-Module["publishMessage"] = function publishMessage(message, topic_name)
-{
-    console.log("  [JS] Received " + message);
-    return 0;
-}
+  // Register new participant with main
+  self.postMessage({
+    command: "register",
+    topic: topic_name,
+    role: role,
+    gid: gid,
+  });
 
-Module["retrieveMessage"] = async function retrieveMessage(topic_name)
-{
-    console.log("  [JS] Listening for messages");
+  return gid;
+};
+
+Module["deregisterParticipant"] = function deregisterParticipant(gid, role) {
+  // Deregister participant from main
+  self.postMessage({
+    command: "deregister",
+    topic: topic,
+    role: role,
+    gid: gid,
+  });
+
+  // TODO: check deregristration
+  return true;
+};
+
+Module["publishMessage"] = function publishMessage(message, topic_name) {
+  // Send message to main
+  self.postMessage({
+    command: "publish",
+    topic: topic_name,
+    message: message,
+  });
+
+  // Assume it gets published
+  return true;
+};
+
+Module["retrieveMessage"] = async function retrieveMessage(topic_name) {
+  if (topic_name in msgMap && !msgMap[topic_name].isStale) {
+    msgMap[topic_name].isStale = true;
+    return msgMap[topic_name].message;
+  } else {
+    // Trigger main to send new message
+    self.postMessage({
+      command: "retrieve",
+      topic: topic_name,
+    });
+
     await sleep(10);
-    return "lastMessage";
-}
-
+    return "";
+  }
+};
