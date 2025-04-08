71ea23e5b75c02e12cdbbddaca314c709f15702d
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2025-04-08 16:16:17 +1000

    cached the result

--- wasm_cpp/include/wasm_cpp/modes.hpp
+++ wasm_cpp/include/wasm_cpp/modes.hpp
@@ -1,34 +1,34 @@
 #include <emscripten.h>
 #include <emscripten/console.h>
-#include <string>
 #include <iostream>
+#include <string>
 #ifndef WASM_CPP__MODES_HPP_
 #define WASM_CPP__MODES_HPP_
 
 inline bool roslibjs_enable() {
-    const char* enabled = std::getenv("ROSLIBJS_ENABLE");
-    if(enabled == nullptr){
-        EM_ASM({console.log("ROSLIBJS_ENABLE not set, defaulting to off print using EM_ASM");});
-        std::cout << "ROSLIBJS_ENABLE not set, version cout" << std::endl; // std::endl flushes the buffer
-        emscripten_out("ROSLIBJS_ENABLE Should be 1 or 0, defaulting to off");
-        return false; //Default off
-    }
-    std::string value(enabled);
-    if (value == "1") {
-
-        emscripten_out("ROSLIBJS_ENABLE 1");
-        return true;
-    } else if (value == "0") {
-
-        emscripten_out("ROSLIBJS_ENABLE 0");
-        return false;
-    }
-    else{
-        std::cout << "ROSLIBJS_ENABLE Should be 1 or 0, defaulting to off cout version";
-        emscripten_out("ROSLIBJS_ENABLE Should be 1 or 0, defaulting to off");
-        EM_ASM({console.log("ROSLIBJS_ENABLE Should be 1 or 0, defaulting to off");});
-        return false;
-    }
+  static bool initialized = false;
+  static bool result = false;
+  if (initialized) {
+    return result;
+  }
+  const char *enabled = std::getenv("ROSLIBJS_ENABLE");
+  initialized = true;
+  if (enabled == nullptr) {
+    emscripten_out("ROSLIBJS_ENABLE Should be 1 or 0, defaulting to off");
+    return false;
+  }
+  std::string value(enabled);
+  if (value == "1") {
+    emscripten_out("ROSLIBJS_ENABLE 1");
+    result = true;
+    return true;
+  } else if (value == "0") {
+    emscripten_out("ROSLIBJS_ENABLE 0");
+    return false;
+  } else {
+    emscripten_out("ROSLIBJS_ENABLE Should be 1 or 0, defaulting to off");
+    return false;
+  }
 }
 
 #endif
